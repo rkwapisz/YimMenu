4fbe19a6371659fdcc851e6bb7bbbe09c56fd5fd
Robert K. <rkkwapisz@gmail.com>
2024-04-12 21:26:51 -0700

    Remove duplicate entries
    Whoops. Forgot to remove redundant code, resulting in duplicate menu entries.

--- src/views/players/player/player_info.cpp
+++ src/views/players/player/player_info.cpp
@@ -73,11 +73,6 @@ namespace big
 				ped_maxhealth   = ped->m_maxhealth;
 			}
 
-			ImGui::Checkbox("VIEW_PLAYER_INFO_BLOCK_EXPLOSIONS"_T.data(), &g_player_service->get_selected()->block_explosions);
-			ImGui::Checkbox("VIEW_PLAYER_INFO_BLOCK_CLONE_CREATE"_T.data(), &g_player_service->get_selected()->block_clone_create);
-			ImGui::Checkbox("VIEW_PLAYER_INFO_BLOCK_CLONE_SYNC"_T.data(), &g_player_service->get_selected()->block_clone_sync);
-			ImGui::Checkbox("VIEW_PLAYER_INFO_BLOCK_NETWORK_EVENTS"_T.data(), &g_player_service->get_selected()->block_net_events);
-
 			components::options_modal(
 			    "VIEW_PLAYER_INFO_EXTRA_INFO"_T.data(),
 			    [ped_health, ped_maxhealth] {
