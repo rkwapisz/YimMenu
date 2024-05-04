ce8ad300b6a4af3dcb28d0df9ac2a91dcfaa0d31
Robert K. <rkkwapisz@gmail.com>
2024-05-04 16:36:12 -0700

    Formatting fixes in infinite_veh_ammo

--- src/backend/looped/vehicle/infinite_veh_ammo.cpp
+++ src/backend/looped/vehicle/infinite_veh_ammo.cpp
@@ -8,10 +8,9 @@ namespace big
 		using looped_command::looped_command;
 
 		virtual void on_tick() override
-        	{
+		{
 			if (self::veh && VEHICLE::DOES_VEHICLE_HAVE_WEAPONS(self::veh))
-            		{
-
+			{
 				for (int i = 0; i < 3; i++)
 				{
 					VEHICLE::SET_VEHICLE_WEAPON_RESTRICTED_AMMO(self::veh, i, -1);
@@ -19,8 +18,8 @@ namespace big
 
 				VEHICLE::SET_VEHICLE_BOMB_AMMO(self::veh, -1);
 				VEHICLE::SET_VEHICLE_COUNTERMEASURE_AMMO(self::veh, -1);
-            		}
-        	}
+			}
+		}
 
 		virtual void on_disable() override
 		{
@@ -37,5 +36,6 @@ namespace big
 		}
 	};
 
-	infinite_veh_ammo g_infinite_veh_ammo("infinitevehammo", "VEHICLE_INFINITE_AMMO", "VEHICLE_INFINITE_AMMO_DESC", g.vehicle.infinite_veh_ammo);
+	infinite_veh_ammo
+	    g_infinite_veh_ammo("infinitevehammo", "VEHICLE_INFINITE_AMMO", "VEHICLE_INFINITE_AMMO_DESC", g.vehicle.infinite_veh_ammo);
 }
