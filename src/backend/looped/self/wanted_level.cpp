dad865334fdfd479573cb1185e3545f1d20101bd
Robert K. <rkkwapisz@gmail.com>
2024-05-21 11:49:49 -0700

    Clang

--- src/backend/looped/self/wanted_level.cpp
+++ src/backend/looped/self/wanted_level.cpp
@@ -1,10 +1,13 @@
 #include "backend/command.hpp"
 #include "backend/looped/looped.hpp"
 #include "backend/looped_command.hpp"
+#include "core/scr_globals.hpp"
+#include "gta_util.hpp"
+#include "util/misc.hpp"
 
 namespace big
 {
-	bool user_updated_wanted_level   = false;
+	bool user_updated_wanted_level = false;
 
 	class clear_wanted : command
 	{
@@ -33,6 +36,15 @@ namespace big
 
 		virtual void on_tick() override
 		{
+			// Disable never wanted if the script needs to force-modify the wanted level in heists
+			if (auto script = gta_util::find_script_thread("fm_mission_controller"_J))
+			{
+				if (!misc::has_bit_set(
+				        script_local(script, scr_locals::fm_mission_controller::mission_controller_wanted_state_flags).as<PINT>(),
+				        6))
+					return;
+			}
+
 			// Clear current wanted level
 			g_local_player->m_player_info->m_wanted_level = 0;
 			g_local_player->m_player_info->m_is_wanted    = false;
